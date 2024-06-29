671d25143d9b1fcaf4de1376ce4239e1d4e765b6
Nans PHILIP <nansphilip@gmail.com>
2024-06-29 13:20:08 +0200

    Push

--- prisma/sql/reset.sql
+++ prisma/sql/reset.sql
@@ -2,16 +2,16 @@
 SELECT User, Host FROM mysql.user;
 
 -- Supprimer un utilisateur
--- DROP USER 'simple-task-user'@'localhost';
+DROP USER 'simple-task-user'@'localhost';
 
 -- Afficher les privilèges d'un utilisateur
 SHOW GRANTS FOR 'simple-task-user'@'localhost';
 
 -- Supprimer tous les privilèges d'un utilisateur
--- REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'simple-task-user'@'localhost';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'simple-task-user'@'localhost';
 
 -- Assurer la mise à jour des privilèges après leur suppression
 FLUSH PRIVILEGES;
 
 -- Supprimer la base de donnée
--- DROP DATABASE `simple-task-db`;
+DROP DATABASE `simple-task-db`;
