7d75bc8f58fe1daf4b85207fb222d8b42f6ebed1
Nans PHILIP <nansphilip@gmail.com>
2024-06-29 13:17:17 +0200

    Push

--- prisma/sql/database.sql
+++ prisma/sql/database.sql
@@ -9,7 +9,3 @@ CREATE USER 'simple-task-user'@'localhost' IDENTIFIED BY 'simple-task-password';
 
 -- Allows user to connect to database
 GRANT ALL PRIVILEGES ON *.* TO 'simple-task-user'@'localhost';
-
--- Adds grants on each tables
-GRANT SELECT, INSERT, UPDATE, DELETE ON `simple-task-db`.`user` TO 'simple-task-user'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `simple-task-db`.`task` TO 'simple-task-user'@'localhost';
